e0329347293e8c0931c68562d7d4250f4df9f0af
daniya418 <101710651+daniya418@users.noreply.github.com>
2025-05-12 14:46:49 +0530

    Implement code changes to enhance functionality and improve performance

---
+++ Basic Declarations and Expressions/1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Name   : Danilka Jayasooriya\n");
+    printf("DOB    : 23/07/2000\n");
+    printf("Mobile : 0xx xxx xxxx\n");
+  
+    return ;
+}
